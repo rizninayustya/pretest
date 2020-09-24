94b5df2ad9477079ca1eafd0513b7553acdef930
rizninayustya <45626486+rizninayustya@users.noreply.github.com>
2020-09-24 14:27:29 +0700

    Add files via upload

---
+++ Pretest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pretest;
+
+/**
+ *
+ * @author RIZNIN
+ */
+public class Pretest {
+
+    public static void main(String[] args) {
+    
+    /* int a = 100+3*5-10/5;
+    System.out.println(a);
+    //*/
+        
+    /*int a = 0;
+    for (int i=0; i<6; ++i){
+         a=a+2;
+    }
+    System.out.println(a/6);
+    //*/
+        
+        String name, address;
+        
+        name = "Jack Donovan";
+        address = "3566 Golden Street Miami";
+        System.out.println(address.replace(" ", " "));
+    }
+}
